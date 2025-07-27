85273fbb9c09f038fc60f6653a035d2921d8c207
RuizMiguel-A <ruizmigueldev@gmail.com>
2025-07-27 17:38:55 -0500

    Parte front en django v1.2

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 
+from django.shortcuts import render
+
 # Create your views here.
 def hello(request):
-    return HttpResponse("Hello, world!")
+    return render(request, 'index.html')
