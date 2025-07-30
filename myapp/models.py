0269a485975f46bd4b385c4332602edc8a69f31a
RuizMiguel-A <ruizmigueldev@gmail.com>
2025-07-30 10:43:09 -0500

    Models v2.0

--- myapp/models.py
+++ myapp/models.py
@@ -6,12 +6,12 @@ class Evento(models.Model):
     nombre = models.CharField(max_length=200)
     fecha = models.DateField()
     hora = models.TimeField()
-    ubicacion = models.CharField(max_length=100) # Podríamos hacer esto un ChoiceField si la lista es fija y pequeña
-    tipo = models.CharField(max_length=50, default='presencial') # 'presencial' o 'virtual'
+    ubicacion = models.CharField(max_length=100)
+    tipo = models.CharField(max_length=50, default='presencial') 
     descripcion = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.nombre
 
     class Meta:
-        ordering = ['fecha', 'hora'] # Ordenar eventos por fecha y hora por defecto
+        ordering = ['fecha', 'hora'] 
