5e4a5663d2b48cda67419a7942a03dfa9c79a2ef
RuizMiguel-A <ruizmigueldev@gmail.com>
2025-07-30 10:45:40 -0500

    Urls v2.0

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,8 +4,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.lista_eventos, name='lista_eventos'), # Tu CRUD principal
-    path('inicio/', views.inicio, name='inicio'), # Nueva URL para la página de inicio
+    path('', views.lista_eventos, name='lista_eventos'), #CRUD principal
+    path('inicio/', views.inicio, name='inicio'), #URL para la página de inicio
     path('editar/<int:pk>/', views.editar_evento, name='editar_evento'),
     path('eliminar/<int:pk>/', views.eliminar_evento, name='eliminar_evento'),
 ]
